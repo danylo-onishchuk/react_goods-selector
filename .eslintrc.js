27bd0b206ca3eaff525f1b51c9b63c83f7eceac9
Danylo Onishchuk <78209611+danylo-onishchuk@users.noreply.github.com>
2021-11-02 17:11:23 +0200

    test
    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 // Uncomment eslint-config-react if you want use react without TS
-
+test
 module.exports = {
   extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
   // extends: ['@mate-academy/eslint-config-react', 'plugin:cypress/recommended'],
